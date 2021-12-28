7080f3ecb3a92c73831ed2c89fd6ba790d5999e3
antonio-oliva <antonio_oliva@outlook.it>
2021-12-28 10:18:33 +0100

    Airline project files added, initialization

---
+++ flights/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+
+from .models import Airport, Flight, Passenger
+
+
+# Register your models here.
+
+# Setting for view Flight(s) in the admin page
+class FlightAdmin(admin.ModelAdmin):
+    list_display = ("id", "origin", "destination", "duration")
+
+
+class PassengerAdmin(admin.ModelAdmin):
+    filter_horizontal = ("flights", )
+
+
+admin.site.register(Airport)
+admin.site.register(Flight, FlightAdmin)
+admin.site.register(Passenger, PassengerAdmin)
